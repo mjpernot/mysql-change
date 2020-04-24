19430e287ca61ba2dd9643ba8f5abda70eb98b43
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2020-04-24 13:42:16 +0100

    Add test/unit/mysql_rep_change/create_instances.py v3.0.2

---
+++ test/unit/mysql_rep_change/create_instances.py
@@ -0,0 +1,160 @@
+#!/usr/bin/python
+# Classification (U)
+
+"""Program:  create_instances.py
+
+    Description:  Unit testing of create_instances in mysql_rep_change.py.
+
+    Usage:
+        test/unit/mysql_rep_change/create_instances.py
+
+    Arguments:
+
+"""
+
+# Libraries and Global Variables
+
+# Standard
+import sys
+import os
+
+if sys.version_info < (2, 7):
+    import unittest2 as unittest
+else:
+    import unittest
+
+# Third-party
+import mock
+
+# Local
+sys.path.append(os.getcwd())
+import lib.gen_libs as gen_libs
+import mysql_rep_change
+import version
+
+__version__ = version.__version__
+
+
+class MasterRep(object):
+
+    """Class:  MasterRep
+
+    Description:  Class stub holder for mysql_class.MasterRep class.
+
+    Methods:
+        __init__ -> Class initialization.
+        upd_mst_status -> upd_mst_status method.
+
+    """
+
+    def __init__(self):
+
+        """Method:  __init__
+
+        Description:  Class initialization.
+
+        Arguments:
+
+        """
+
+        self.name = "Server_Name"
+        self.read_only = "OFF"
+        self.server_id = 10
+        self.sql_user = "User"
+        self.sql_pass = None
+        self.machine = "Linux"
+        self.host = "HostName"
+        self.port = 3306
+        self.defaults_file = None
+
+    def upd_mst_status(self):
+
+        """Method:  upd_mst_status
+
+        Description:  upd_mst_status method.
+
+        Arguments:
+
+        """
+
+        return True
+
+
+class SlaveRep(object):
+
+    """Class:  SlaveRep
+
+    Description:  Class stub holder for mysql_class.SlaveRep class.
+
+    Methods:
+        __init__ -> Class initialization.
+
+    """
+
+    def __init__(self):
+
+        """Method:  __init__
+
+        Description:  Class initialization.
+
+        Arguments:
+
+        """
+
+        self.name = "Server_Name"
+        self.read_only = "OFF"
+        self.server_id = 10
+        self.sql_user = "User"
+        self.sql_pass = None
+        self.machine = "Linux"
+        self.host = "HostName"
+        self.port = 3306
+        self.defaults_file = None
+
+
+class UnitTest(unittest.TestCase):
+
+    """Class:  UnitTest
+
+    Description:  Class which is a representation of a unit testing.
+
+    Methods:
+        setUp -> Initialize testing environment.
+        test_create_instances -> Test create_instances function.
+
+    """
+
+    def setUp(self):
+
+        """Function:  setUp
+
+        Description:  Initialization for unit testing.
+
+        Arguments:
+
+        """
+
+        self.args_array = {"-c": "mysql_cfg", "-d": "config", "-s": "slave"}
+
+    @mock.patch("mysql_rep_change.mysql_libs.create_slv_array",
+                mock.Mock(return_value="SlaveArray"))
+    @mock.patch("mysql_rep_change.cmds_gen.create_cfg_array",
+                mock.Mock(return_value=[]))
+    @mock.patch("mysql_rep_change.mysql_libs.create_instance",
+                mock.Mock(return_value="Master"))
+    def test_create_instances(self):
+
+        """Function:  test_create_instances
+
+        Description:  Test create_instances function.
+
+        Arguments:
+
+        """
+
+        self.assertEqual(mysql_rep_change.create_instances(
+            self.args_array), ("Master", "SlaveArray"))
+
+
+if __name__ == "__main__":
+    unittest.main()
