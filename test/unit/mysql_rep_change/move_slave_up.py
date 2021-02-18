10fc7468a42181d86b968cb9445a9bdab1bb12fa
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2021-02-18 13:25:15 +0000

    Updated test/unit/mysql_rep_change/move_slave_up.py v3.0.3

--- test/unit/mysql_rep_change/move_slave_up.py
+++ test/unit/mysql_rep_change/move_slave_up.py
@@ -29,6 +29,7 @@ import mock
 # Local
 sys.path.append(os.getcwd())
 import mysql_rep_change
+import lib.gen_libs as gen_libs
 import version
 
 __version__ = version.__version__
@@ -68,6 +69,8 @@ class MasterRep(object):
         self.rep_user = "RepUser"
         self.rep_japd = None
         self.defaults_file = None
+        self.conn = True
+        self.conn_msg = None
 
     def upd_mst_status(self):
 
@@ -127,6 +130,8 @@ class SlaveRep(object):
         self.rep_user = "RepUser"
         self.rep_japd = None
         self.defaults_file = None
+        self.conn = True
+        self.conn_msg = None
 
     def connect(self):
 
@@ -182,6 +187,9 @@ class UnitTest(unittest.TestCase):
 
     Methods:
         setUp -> Initialize testing environment.
+        test_no_slave_new_master -> Test with no new & slave master connection.
+        test_no_slave_master -> Test with no slave master connection fails.
+        test_no_new_master -> Test with no new master connection fails.
         test_slave_moved -> Test with slave moved up.
         test_find_slave_fails -> Test with find of slave fails.
         test_sync_slave_fails -> Test with sync up of slave fails.
@@ -205,8 +213,102 @@ class UnitTest(unittest.TestCase):
         self.slaves = [self.slave]
         self.err_msg = "Error: Sync Rep Failed"
         self.err_msg2 = "Error: Sync Replication2 Failed"
+        self.err_msg3 = "Connection error"
+        self.err_msg4 = "Detected problem in one of the connections"
         self.new_mst = "NewMaster"
         self.args = {"-d": True}
+
+    @mock.patch("mysql_rep_change.cmds_gen.disconnect",
+                mock.Mock(return_value=True))
+    @mock.patch("mysql_rep_change.fetch_slv",
+                mock.Mock(return_value=("SlaveMove", False, None)))
+    @mock.patch("mysql_rep_change.mysql_class.SlaveRep")
+    @mock.patch("mysql_rep_change.gen_libs.load_module")
+    @mock.patch("mysql_rep_change.mysql_class.MasterRep")
+    def test_no_slave_new_master(self, mock_inst, mock_cfg, mock_slv):
+
+        """Function:  test_no_slave_new_master
+
+        Description:  Test with no new & slave master connection.
+
+        Arguments:
+
+        """
+
+        self.master.conn = False
+        self.master.conn_msg = self.err_msg3
+        self.slave.conn = False
+        self.slave.conn_msg = self.err_msg3
+
+        mock_inst.return_value = self.master
+        mock_cfg.return_value = self.cfg
+        mock_slv.return_value = self.slave
+
+        with gen_libs.no_std_out():
+            self.assertEqual(
+                mysql_rep_change.move_slave_up(
+                    self.master, self.slaves, args=self.args,
+                    new_mst=self.new_mst), (True, self.err_msg4))
+
+    @mock.patch("mysql_rep_change.cmds_gen.disconnect",
+                mock.Mock(return_value=True))
+    @mock.patch("mysql_rep_change.fetch_slv",
+                mock.Mock(return_value=("SlaveMove", False, None)))
+    @mock.patch("mysql_rep_change.mysql_class.SlaveRep")
+    @mock.patch("mysql_rep_change.gen_libs.load_module")
+    @mock.patch("mysql_rep_change.mysql_class.MasterRep")
+    def test_no_slave_master(self, mock_inst, mock_cfg, mock_slv):
+
+        """Function:  test_no_slave_master
+
+        Description:  Test with no slave master connection fails.
+
+        Arguments:
+
+        """
+
+        self.slave.conn = False
+        self.slave.conn_msg = self.err_msg3
+
+        mock_inst.return_value = self.master
+        mock_cfg.return_value = self.cfg
+        mock_slv.return_value = self.slave
+
+        with gen_libs.no_std_out():
+            self.assertEqual(
+                mysql_rep_change.move_slave_up(
+                    self.master, self.slaves, args=self.args,
+                    new_mst=self.new_mst), (True, self.err_msg4))
+
+    @mock.patch("mysql_rep_change.cmds_gen.disconnect",
+                mock.Mock(return_value=True))
+    @mock.patch("mysql_rep_change.fetch_slv",
+                mock.Mock(return_value=("SlaveMove", False, None)))
+    @mock.patch("mysql_rep_change.mysql_class.SlaveRep")
+    @mock.patch("mysql_rep_change.gen_libs.load_module")
+    @mock.patch("mysql_rep_change.mysql_class.MasterRep")
+    def test_no_new_master(self, mock_inst, mock_cfg, mock_slv):
+
+        """Function:  test_no_new_master
+
+        Description:  Test with no new master connection fails.
+
+        Arguments:
+
+        """
+
+        self.master.conn = False
+        self.master.conn_msg = self.err_msg3
+
+        mock_inst.return_value = self.master
+        mock_cfg.return_value = self.cfg
+        mock_slv.return_value = self.slave
+
+        with gen_libs.no_std_out():
+            self.assertEqual(
+                mysql_rep_change.move_slave_up(
+                    self.master, self.slaves, args=self.args,
+                    new_mst=self.new_mst), (True, self.err_msg4))
 
     @mock.patch("mysql_rep_change.is_slv_up", mock.Mock(return_value=True))
     @mock.patch("mysql_rep_change.mysql_libs.chg_slv_state",
